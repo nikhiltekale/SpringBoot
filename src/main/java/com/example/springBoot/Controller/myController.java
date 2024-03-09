1c31e5a0ad9e1c433881b20cbcd3305e7811bf0f
nikhilTekale <nikhiltekale45@gmail.com>
2024-03-09 14:15:19 +0530

    Added controller file in application

---
+++ src/main/java/com/example/springBoot/Controller/myController.java
@@ -0,0 +1,12 @@
+package com.example.springBoot.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class myController {
+    @GetMapping("/first")
+    public String run(){
+        return "this is first springBoot project in Intellij idea";
+    }
+}
